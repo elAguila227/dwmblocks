791393c7b970cba490a7333b5e5362030967f702
Alexander Zerpa <alexzerpawanner@gmail.com>
2021-05-26 13:43:37 +0200

    changed delimiter.

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static const Block blocks[] = {
 	/* {"",	"sb-moonphase",	18000,	17}, */
 	/* {"",	"sb-forecast",	18000,	5}, */
 	/* {"",	"sb-mailbox",	180,	12}, */
-	{"",	"sb-nettraf",	1,	16},
+	/* {"",	"sb-nettraf",	1,	16}, */
 	/* {"",	"sb-volume",	0,	10}, */
 	{" ",	"sb-battery",	5,	3},
 	{"",	"sb-clock",	60,	1},
@@ -28,7 +28,7 @@ static const Block blocks[] = {
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char *delim = " ";
+static char *delim = " | ";
 
 // Have dwmblocks automatically recompile and run when you edit this file in
 // vim with the following line in your vimrc/init.vim:
